b4fa16c924f805a795854482a809641da950b4a5
royalcaster <gabriel.pechstein@schneeberg.km3.de>
2021-08-12 23:43:41 +0200

    12.08.2021

--- js/workplace.js
+++ js/workplace.js
@@ -1,9 +1,12 @@
 // Make all the DIV elements draggable:
 var containers = document.getElementsByClassName("snippet_container");
 
-for(var i = 0; i < containers.length; i++) {
-  dragSnippet(containers[i]);
+function updateDrag(){
+  for(var i = 0; i < containers.length; i++) {
+    dragSnippet(containers[i]);
+  }
 }
+
 
 //drag-funktion für Snippet
 function dragSnippet(elmnt) {
@@ -47,18 +50,73 @@ function dragSnippet(elmnt) {
   }
 }
 
-function addSnippet(x,y,type) {
-    var Snippet = createSnippet(x,y,type);
+function createNote() {
+  let snippet_container = createSnippetContainer("note");
+
+  var content_box = document.createElement("div");
+  content_box.className = "content_box";
+  
+  var text_area = document.createElement("textarea");
+  text_area.className = "note_textarea";
+  text_area.spellcheck = false;
+  text_area.placeholder = "Ihre Notiz..." 
+
+  content_box.appendChild(text_area);
+  snippet_container.getElementsByClassName("snippet_body")[0].appendChild(content_box);
+  document.getElementById("workplace_container").appendChild(snippet_container);
+  updateDrag();
 }
 
-function createSnippet(x,y,type) {
-  const Note = {
-    x: x,
-    y: y,
-    type: type
+function createSnippetContainer(type){
+  var snippet_container = document.createElement("div");
+  snippet_container.className = "snippet_container";
+
+  var snippet_header = document.createElement("div");
+  snippet_header.className = "snippet_header";
+
+  var h_lines = document.createElement("div");
+  h_lines.className = "h_lines";
+
+  var h_line = document.createElement("hr");
+  h_line.className = "h_line";
+
+  var snippet_body = document.createElement("div");
+  snippet_body.className = "snippet_body";
+
+  var title_box = document.createElement("div");
+  title_box.className = "title_box";
+
+  var icon = document.createElement("i");
+
+  switch (type) {
+  case "note":
+    icon.className = "material-icons snippet_icon icon_note";
+    icon.innerHTML = "&#xe06f;";
+    break;
+  case "table":
+    icon.className = "material-icons snippet_icon icon_table";
+    icon.innerHTML = "&#xe3ec;";
+    break;
+  case "list":
+    icon.className = "material-icons snippet_icon icon_list";
+    icon.innerHTML = "&#xe241;";
+    break;
   }
-}
 
-function placeSnippet(snippet){
-    
+  var title_field = document.createElement("input");
+  title_field.className = "title_field";
+  title_field.spellcheck = false;
+  title_field.placeholder = "Unbenannt";
+
+  h_lines.appendChild(h_line);
+  snippet_header.appendChild(h_lines);
+  snippet_container.appendChild(snippet_header);
+  title_box.appendChild(icon);
+  title_box.appendChild(title_field);
+  snippet_body.appendChild(title_box);
+  snippet_container.appendChild(snippet_body);
+  //document.getElementById("workplace_container").appendChild(snippet_container);
+  return snippet_container;
+
+  
 }
